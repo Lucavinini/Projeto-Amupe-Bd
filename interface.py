34d988cb77fec1972abca03723cbd4a529e707e2
Lucavinini <lucas.00000850855@unicap.br>
2025-08-10 17:52:12 -0300

    Organizando..

---
+++ interface.py
@@ -0,0 +1,9 @@
+
+def Interface():
+    print("____________________________")
+    print("1. Inserir um novo prefeito")
+    print("2. Editar os dados de um prefeito")
+    print("3. Buscar")
+    print("4. Exibir os dados em CSV")
+    print("____________________________")
+
--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 from db import supabase
 from crud import InserirPrefeito
+from interface import Interface
 
-InserirPrefeito(
-    municipio="Abreu e Lima",
-    regiao="Nordeste",
-    populacao=50000,
-    prefeito="João da Silva",
-    cpf="12345678900",
-    partido="ABC",
-    status="eleito",
-    telefone="(87) 99999-9999",
-    email="joao@example.com",
-    esposas="Maria da Silva"
-)
+a = 0
+
+while a != 5:
+
+    Interface()
+    a = int(input())
+
+    if a == 1:
+        muni = input("Digite o municipio: ")
+        regi = input("Digite a região: ")
+        popu = input("Digite a população: ")
+        nome = input("Digite um nome: ")
+        cpfi = input("Digite o CPF: ")
+        part = input("Digite o partido: ")
+        stat = input("Digite o status (Eleito/Não eleito): ")
+        tel = input("Digite o telefone: ")
+        emai = input("Digite o email: ")
+        espo = input("Digite o nome da esposa: \n")
+
+        InserirPrefeito(muni,regi,popu, nome, cpfi, part, stat, tel,
+                        emai, espo)
+
+    
+    
+
+    
+    
+
+    
+
 
 
