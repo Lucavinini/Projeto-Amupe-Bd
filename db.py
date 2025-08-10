f409755372c8eef1e6e2c984bed08710e134a653
Lucavinini <lucas.00000850855@unicap.br>
2025-08-10 11:22:17 -0300

    Criando a minha primeira função de inserção no bd

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 from supabase import create_client
 
-# Substitua pelos seus valores do passo 3
 SUPABASE_URL = "https://ryvvjenayolflezqjogs.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5dnZqZW5heW9sZmxlenFqb2dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3NzI2MzEsImV4cCI6MjA3MDM0ODYzMX0.6-cuQcTdNIBnxR2VO38y7I15TYXfKN_OiasZxeAy_U4"
 
